f2c3330ba914a2f5fdc7ff4704e5cd3c8408c89a
jmikelel <161890739+jmikelel@users.noreply.github.com>
2024-03-01 16:50:05 -0600

    2nd cOMMIT

--- filtering.py
+++ filtering.py
@@ -4,3 +4,9 @@
 import numpy as np
 import cv2
 import sys
+
+if __name__ == '__main__':    
+# Ask the user to enter the input image name
+    args = parse_user_data()   
+    # Run pipeline  
+    run_pipeline(args=args)
